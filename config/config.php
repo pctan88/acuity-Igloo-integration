6977d578392e3702dc3496764b9c4a92e31b386e
pctan88 <pctan88@users.noreply.github.com>
2024-10-08 00:43:49 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -4,8 +4,11 @@ return [
     'acuity_user_id' => 'your-user-id',
     'acuity_api_key' => 'your-api-key',
     'igloo_api_url' => 'https://api.igloodeveloper.co/igloohome/devices/{deviceId}/jobs/bridges/{bridgeId}',
+    'igloo_token_url' => 'https://auth.igloohome.co/oauth2/token',
     'igloo_access_token' => 'your-igloo-access-token',
     'igloo_device_id' => 'your-device-id',
     'igloo_bridge_id' => 'your-bridge-id',
+    'igloo_client_id' => 'your-client_id',
+    'igloo_client_secret' => 'your-client_secret',
     'log_file' => __DIR__ . '/../logs/cron.log',
 ];
